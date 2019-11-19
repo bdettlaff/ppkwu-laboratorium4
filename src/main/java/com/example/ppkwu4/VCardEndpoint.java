5957ba7e70567349f3eeaa8c00bf5d549c17bb50
209295 <bartlomiejdettlaff@interia.pl>
2019-11-19 17:03:00 +0100

    Praca praca

--- src/main/java/com/example/ppkwu4/VCardEndpoint.java
+++ src/main/java/com/example/ppkwu4/VCardEndpoint.java
@@ -1,4 +1,13 @@
 package com.example.ppkwu4;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class VCardEndpoint {
+
+    @GetMapping("/find")
+    public String find(){
+        return "find";
+    }
 }
